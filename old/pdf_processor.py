70fe3d41b73191bac98a1053eff208b78404cdc1
Kamalakanta <kamala.sethi@gmail.com>
2024-07-21 15:26:38 +0530

    delete old file

--- old/pdf_processor.py
+++
@@ -1,9 +0,0 @@
-import PyPDF2
-
-def extract_text_from_pdf(pdf_path):
-    with open(pdf_path, 'rb') as file:
-        pdf_reader = PyPDF2.PdfFileReader(file)
-        text = ""
-        for page_num in range(len(pdf_reader.pages)):
-            text += pdf_reader.pages[page_num].extract_text()
-    return text
--- old/utils.py
+++
@@ -1,5 +0,0 @@
-# import os
-# import openai # type: ignore
-
-# def set_openai_key():
-#   openai.api_key = os.environ.get("OPENAI_API_KEY")
